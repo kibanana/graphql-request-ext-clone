8a2dfe6cad51f22a0fed6abf585a99f6c25ebb53
김예원 <kyw017763@gmail.com>
2019-10-28 17:25:21 +0900

    Change code structure

--- graphql-query-prac/out/extension.js
+++ graphql-query-prac/out/extension.js
@@ -9,27 +9,329 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const vscode_1 = require("vscode");
 const vscode = require("vscode");
+const graphql_request_1 = require("graphql-request");
+const hljs = require("highlight.js");
+const codeHighlightLinenums = require("code-highlight-linenums");
 const queryInput_1 = require("./queryInput");
 function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.helloGraphQL', () => __awaiter(this, void 0, void 0, function* () {
-        const options = {
-            showInputBoxMine: queryInput_1.showInputBoxMine
-        };
-        const quickPick = vscode.window.createQuickPick();
-        quickPick.items = Object.keys(options).map(label => ({ label }));
-        quickPick.onDidChangeSelection(selection => {
-            if (selection[0]) {
-                options[selection[0].label](context)
-                    .catch(console.error);
-            }
+        queryInput_1.showInputBoxMine.call(() => {
+        }).then(data => {
+            console.log(`${data[0]}, ${data[1]}, ${data[2]}`);
+            makeWebview(data[0], data[1], data[2]);
         });
-        quickPick.onDidHide(() => quickPick.dispose());
-        quickPick.show();
     })));
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
+function makeWebview(URL, QUERY, VARIABLE) {
+    if (!(URL && QUERY)) {
+        vscode.window.showErrorMessage(`Can't Execute Query!`);
+        return;
+    }
+    let panel = vscode_1.window.createWebviewPanel('graphqlResponse', 'Graphql Response', vscode.ViewColumn.Two);
+    if (VARIABLE) {
+        graphql_request_1.request(URL, QUERY, JSON.parse(VARIABLE))
+            .then((res) => {
+            console.log(JSON.stringify(res, null, 4));
+            panel.webview.html = getWebviewContent(res);
+        });
+    }
+    else {
+        graphql_request_1.request(URL, QUERY)
+            .then((res) => {
+            console.log(JSON.stringify(res, null, 4));
+            panel.webview.html = getWebviewContent(res);
+        });
+    }
+}
+function getWebviewContent(response) {
+    let formattedCode = codeHighlightLinenums(JSON.stringify(response, null, 2), {
+        hljs,
+        lang: 'json',
+        start: 1
+    });
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <style>
+            @charset "UTF-8";
+
+            body.vscode-light {
+            background-color: inherit;
+            }
+
+            body.vscode-dark {
+            background-color: inherit;
+            }
+
+            body.vscode-high-contrast {
+            background-color: inherit;
+            }
+
+            pre .hljs-comment {
+            color: #57A64A;
+            font-style: italic;
+            }
+
+            .vscode-light pre .hljs-attribute {
+            color: #c82829;
+            }
+
+            .vscode-dark pre .hljs-attribute {
+            color: #cc6666;
+            }
+
+            .vscode-high-contrast pre .hljs-attribute {
+            color: #ff9da4;
+            }
+
+            .vscode-light pre .hljs-attr {
+            color: #eab700;
+            }
+
+            .vscode-dark pre .hljs-attr {
+            color: #f0c674;
+            }
+
+            .vscode-high-contrast pre .hljs-attr {
+            color: #ffeead;
+            }
+
+            .vscode-light pre .hljs-number,
+            .vscode-light pre .hljs-literal {
+            color: #f5871f;
+            }
+
+            .vscode-dark pre .hljs-number,
+            .vscode-dark pre .hljs-literal {
+            color: #de935f;
+            }
+
+            .vscode-high-contrast pre .hljs-number,
+            .vscode-high-contrast pre .hljs-literal {
+            color: #ffc58f;
+            }
+
+            .vscode-light pre .hljs-string {
+            color: #718c00;
+            }
+
+            .vscode-dark pre .hljs-string {
+            color: #b5bd68;
+            }
+
+            .vscode-high-contrast pre .hljs-string {
+            color: #d1f1a9;
+            }
+
+            .vscode-light pre .hljs-name,
+            .vscode-dark pre .hljs-name,
+            .vscode-high-contrast pre .hljs-name {
+            color: #007acc;
+            }
+
+            .vscode-light pre .hljs-meta,
+            .vscode-light pre .hljs-tag,
+            .vscode-light pre .hljs-keyword {
+            color: #8959a8;
+            }
+
+            .vscode-dark pre .hljs-meta,
+            .vscode-dark pre .hljs-tag,
+            .vscode-dark pre .hljs-keyword {
+            color: #b294bb;
+            }
+
+            .vscode-high-contrast pre .hljs-meta,
+            .vscode-high-contrast pre .hljs-tag,
+            .vscode-high-contrast pre .hljs-keyword {
+            color: #ebbbff;
+            }
+
+            pre .hljs-emphasis {
+            font-style: italic;
+            }
+
+            pre .hljs-strong {
+            font-weight: bold;
+            }
+
+            code {
+            display: block;
+            background: inherit;
+            font-family: Menlo, Monaco, Consolas, "Droid Sans Mono", "Courier New", monospace, "Droid Sans Fallback";
+            font-size: 13px;
+            line-height: 1.5;
+            padding: 10px;
+            white-space: pre-wrap;
+            word-break: break-all;
+            }
+
+            .vscode-light pre code {
+            color: #4d4d4c;
+            }
+
+            .vscode-dark pre code {
+            color: #c5c8c6;
+            }
+
+            .vscode-high-contrast pre code {
+            color: white;
+            }
+
+            .line .icon {
+            position: absolute;
+            display: inline-block;
+            left: calc(2ch + 3px);
+            top: 0.15em;
+            width: 1em;
+            height: 1em;
+            cursor: pointer;
+            }
+
+            .vscode-light .line .icon {
+            background: url(expand.svg);
+            background-size: 1.2em;
+            background-repeat: no-repeat;
+            }
+
+            .vscode-dark .line .icon,
+            .vscode-high-contrast .line .icon {
+            background: url(expand-dark.svg);
+            background-size: 1.2em;
+            background-repeat: no-repeat;
+            }
+
+            .line.collapsed .icon {
+            position: absolute;
+            display: inline-block;
+            left: calc(2ch + 3px);
+            top: 0.15em;
+            width: 1em;
+            height: 1em;
+            cursor: pointer;
+            }
+
+            .vscode-light .line.collapsed .icon {
+            background: url(collapse.svg);
+            background-size: 1.2em;
+            background-repeat: no-repeat;
+            }
+
+            .vscode-dark .line.collapsed .icon,
+            .vscode-high-contrast .line.collapsed .icon {
+            background: url(collapse-dark.svg);
+            background-size: 1.2em;
+            background-repeat: no-repeat;
+            }
+
+            code .line {
+            display: inline-block;
+            position: relative;
+            padding-left: calc(2ch + 20px );
+            }
+
+            code .line.hidden-line {
+            display: none;
+            }
+
+            code .line:after {
+            content: ' ';
+            }
+
+            code .line.collapsed:after {
+            color: grey;
+            margin: 0.1em 0.2em 0 0.2em;
+            content: "⋯";
+            display: inline;
+            line-height: 1.5;
+            }
+
+            code .line:before {
+            box-sizing: content-box;
+            display: inline-block;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            text-align: right;
+            width: 2ch;
+            content: attr(start);
+            padding-right: 9px;
+            padding-left: 9px;
+            margin-left: calc(-2ch + -30px );
+            margin-right: 9px;
+            color: #787878;
+            background-color: inherit;
+            }
+
+            a {
+            color: #4080D0;
+            text-decoration: none;
+            }
+
+            a:hover {
+            color: #4080D0;
+            text-decoration: underline;
+            }
+
+            /* Copy from https://github.com/Microsoft/vscode-docs/blob/master/release-notes/css/inproduct_releasenotes.css */
+            #scroll-to-top {
+            position: fixed;
+            width: 40px;
+            height: 40px;
+            right: 25px;
+            bottom: 25px;
+            background-color:#444444;
+            border-radius: 50%;
+            cursor: pointer;
+            box-shadow: 1px 1px 1px rgba(0,0,0,.25);
+            }
+
+            #scroll-to-top:hover {
+            background-color:#007acc;
+            box-shadow: 2px 2px 2px rgba(0,0,0,.25);
+            }
+
+            /* Theme-specific colors */
+            body.vscode-light #scroll-to-top {
+            background-color: #949494;
+            }
+            body.vscode-light #scroll-to-top:hover {
+            background-color: #007acc;
+            }
+
+            body.vscode-high-contrast #scroll-to-top {
+            background-color: black;
+            border: 2px solid #6fc3df;
+            box-shadow: none;
+            }
+            body.vscode-high-contrast #scroll-to-top:hover {
+            background-color: #007acc;
+            }
+
+            #scroll-to-top span.icon::before {
+            content: "";
+            /* Chevron up icon */
+            background:url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE5LjIuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgIC0tPgo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IgoJIHZpZXdCb3g9IjAgMCAxNiAxNiIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgMTYgMTY7IiB4bWw6c3BhY2U9InByZXNlcnZlIj4KPHN0eWxlIHR5cGU9InRleHQvY3NzIj4KCS5zdDB7ZmlsbDojRkZGRkZGO30KCS5zdDF7ZmlsbDpub25lO30KPC9zdHlsZT4KPHRpdGxlPnVwY2hldnJvbjwvdGl0bGU+CjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik04LDUuMWwtNy4zLDcuM0wwLDExLjZsOC04bDgsOGwtMC43LDAuN0w4LDUuMXoiLz4KPHJlY3QgY2xhc3M9InN0MSIgd2lkdGg9IjE2IiBoZWlnaHQ9IjE2Ii8+Cjwvc3ZnPgo=');
+            width: 1.2rem;
+            height: 1.2rem;
+            position: absolute;
+            left: calc(50% - 1.2rem / 2);
+            top: calc(50% - 1.2rem / 2);
+            }
+        </style>
+        <title>GraphQL Response</title>
+    </head>
+    <body>
+        <pre><code>${formattedCode}</code>
+        </pre>
+    </body>
+    </html>`;
+}
+exports.getWebviewContent = getWebviewContent;
 //# sourceMappingURL=extension.js.map
